18b3c5b8a98a9614955d7665ceed96c1621092db
David Carmona <davicarm@ucm.es>
2022-11-12 15:11:54 +0100

    grafiti txt matrix read and spawn sprite on mouse position

---
+++ Assets/Scripts/GraffitiMiniGame.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraffitiMiniGame : MonoBehaviour
+{
+    public Texture2D [] tex;
+    private string[] binaries;
+    public Sprite mySprite;
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+        sr.color = new Color(0.9f, 0.9f, 0.9f, 1.0f);
+
+        transform.position = new Vector3(1.5f, 1.5f, 0.0f);
+        binaries = new string[240];
+    }
+
+    void Start()
+    {
+        //mySprite = Sprite.Create(tex[0], new Rect(0.0f, 0.0f, Screen.width, Screen.height), new Vector2(0.5f, 0.5f), 100.0f);
+        sr.sprite = mySprite;
+        sr.transform.position = new Vector3(0, 0, 0);
+        sr.transform.localScale = new Vector3(1,1,1);
+
+        var textFile = Resources.Load<TextAsset>("GrafitiMuelle");
+        binaries = textFile.text.Split('\n');
+
+       // Debug.Log(textFile.text.Length);
+        //for (int i = 0; i < textFile.text.Length; i++)
+        //{
+        //    binaries[i] = textFile.text.Split('\n');
+        //}
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+      
+    }
+}
---
+++ Assets/Scripts/SpawnPoints.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour
+{
+    public GameObject prefab;
+    public float Y;
+
+    private Camera cam;
+    private List<Vector2> points = new List<Vector2>();
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            cam = Camera.main;
+            Vector2 mousePos = new Vector2();
+            mousePos.x = Input.mousePosition.x;
+            mousePos.y = cam.pixelHeight - Input.mousePosition.y;
+            Vector3 point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10));
+            points.Add(new Vector2(point.x, point.y * -1));
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            float totalDistance = 0;
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                totalDistance += Vector2.Distance(points[i], points[i + 1]);
+            }
+            float averageDistance = totalDistance / (points.Count - 1);
+
+            // Spawning first Point
+            Vector2 lastPoint = points[0];
+            Instantiate(
+                prefab,
+                new Vector3(lastPoint.x, lastPoint.y,Y),
+                Quaternion.identity
+            );
+            // Spawning other Points
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                Vector2 spawnPos = lastPoint + ((points[i + 1] - lastPoint).normalized * averageDistance);
+
+                lastPoint = spawnPos; //Save the new point because you want create the next one from this one
+                Instantiate(
+                    prefab,
+                    new Vector3(spawnPos.x, spawnPos.y, Y),
+                    Quaternion.identity
+                );
+            }
+            // Resetting List
+            points = new List<Vector2>();
+        }
+    }
+}
