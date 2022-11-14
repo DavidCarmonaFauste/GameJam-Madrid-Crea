4b3a4f2918a9b02a4f81218025ef16cc351f62d8
ikerdi <ikerdi99@gmail.com>
2022-11-14 18:38:57 +0100

    a merendar
    coches, he tocao algunas cosas del primer callejon y tal

---
+++ Assets/Scripts/VehicleSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject car;
+
+    private float carRate, nextCar;
+
+    [SerializeField]
+    private float minCarRate = 2, maxCarRate = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carRate = Random.Range(minCarRate, maxCarRate);
+        nextCar = Time.time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Time.time > nextCar))
+        {
+
+            Instantiate(car, transform.position, transform.rotation);
+            nextCar = Time.time + carRate;
+        }
+    }
+
+}
---
+++ Assets/Vehicles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicles : MonoBehaviour
+{
+    [SerializeField]
+    private float speed, lifeTime;
+
+     Transform t;
+     Vector2 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GameObject.Find("vehicle_target").transform;
+        target = new Vector2(t.position.x, t.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
